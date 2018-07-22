69a498fae01d2c57715e05556dfe52fde6e6d89d
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2018-07-22 18:52:57 -0400

    test generator works

--- js/app.js
+++ js/app.js
@@ -2,14 +2,19 @@ const app = angular.module('SithApp', []);
 
 app.controller('MainController', function($scope) {
 
-  $scope.names = ["Darth", "Lord", "Master"];
-  $scope.genders = ["Male", "Female"];
-  this.randomName = "Tester";
+  $scope.prefixes = ["Darth", "Lord", "Master"];
+  $scope.funnys = ["Yes", "No"];
+  this.randomName = ["Tester", "Test", "Also a Tester"];
 
   this.ready = false;
 
+  this.reload = () => {
+    location.reload();
+  }
+
   this.done = () => {
     this.ready = true;
+    this.sithName = this.randomName[Math.floor(Math.random() * this.randomName.length)];
   }
 
 
